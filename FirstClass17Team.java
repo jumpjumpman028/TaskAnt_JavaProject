21b03c90da846aadc23f41c7a6d84b4b19f9e11c
jumpjumpman <145435018+jumpjumpman028@users.noreply.github.com>
2025-04-22 18:38:24 +0800

    Invite members.

---
+++ FirstClass17Team.java
@@ -0,0 +1,8 @@
+public class FirstClass17Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257026\"\n姓名：\"蔡孟睿\"");
+        System.out.println("\"學號：\"01257020\"\n姓名：\"張肇豐\"");
+        System.out.println("\"學號: \"00957113\"\n姓名: \"周俊宇\"");
+    }
+}
